6de2d771bb5fdffa190e42ae474a1dd04a557520
Chizea Matthew <matkenefaiths@gmail.com>
2021-06-25 12:07:35 +0100

    Setup for TMDS 25TH JUNE 2021

---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use jeremykenedy\LaravelRoles\Traits\HasRoleAndPermission; // Added too
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+    use HasRoleAndPermission; // Added as request in Laravel-roles installation
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+//Added
+use Illuminate\Database\Eloquent\Model;
+use Database\Seeders\PermissionsTableSeeder;
+use Database\Seeders\RolesTableSeeder;
+use Database\Seeders\ConnectRelationshipsSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        Model::unguard();
+
+            $this->call(PermissionsTableSeeder::class);
+            $this->call(RolesTableSeeder::class);
+            $this->call(ConnectRelationshipsSeeder::class);
+            //$this->call('UsersTableSeeder');
+
+        Model::reguard();
+    }
+}
