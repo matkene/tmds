6926fd037ed9a5b1ce112260cf539d015b3b590b
Robert Ebafua <robertebafua@gmail.com>
2021-07-29 12:14:15 +0100

    Modified Routes

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,11 @@ use App\Http\Controllers\V1\Api\Auth\RegisterController;
 use App\Http\Controllers\v1\Auth\AccountSettingsController;
 use App\Http\Controllers\V1\Api\Auth\VerificationController;
 use App\Http\Controllers\v1\Api\Auth\ForgotPasswordController;
-use App\Http\Controllers\v1\Api\User\EventController AS UserEventController;
+use App\Http\Controllers\v1\Api\User\EventController as UserEventController;
 
 Route::group(['prefix' => 'v1'], function ($router) {
 
-    Route::get('/test', function() {
+    Route::get('/test', function () {
         return 'Hello test 1';
     });
 
@@ -49,15 +49,14 @@ Route::group(['prefix' => 'v1'], function ($router) {
         // Tour Route
         Route::group(["prefix" => "tours"], function () {
             Route::get('/', [TourController::class, 'index']);
-            Route::get('/show/{id}',[TourController::class, 'showTour']);
+            Route::get('/show/{id}', [TourController::class, 'showTour']);
         });
-
     });
 
     // Admin Route
     Route::group(["prefix" => "admin",  "middleware" => ["auth:api", "admin"], "namespace" => "V1\Api\Admin"], function () {
 
-          // Events
+        // Events
         Route::group(["prefix" => "events"], function () {
             Route::get('/', [EventController::class, 'index']);
             Route::post('view-single-event', [EventController::class, 'showEvent']);
@@ -72,7 +71,6 @@ Route::group(['prefix' => 'v1'], function ($router) {
             Route::post('/create', [TourController::class, 'createTour']);
             Route::post('/update', [TourController::class, 'update']);
         });
-
     });
 
     // Guest Route
@@ -82,7 +80,5 @@ Route::group(['prefix' => 'v1'], function ($router) {
         Route::group(["prefix" => "events"], function () {
             Route::get('/', [GuestController::class, 'listAllActiveEvents']);
         });
-
     });
-
 });
