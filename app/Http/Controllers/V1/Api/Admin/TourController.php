791d665be6971df0065a4fa01b1ea616526ef952
Akinwunmi Damilola <akinwunmi.damilola@yahoo.com>
2021-08-05 14:18:12 +0100

    update

--- app/Http/Controllers/V1/Api/Admin/TourController.php
+++ app/Http/Controllers/V1/Api/Admin/TourController.php
@@ -83,6 +83,7 @@ class TourController extends Controller
 
             DB::beginTransaction();
 
+            $images = [];
             if ($files = $request->file('image')) {
                 foreach ($files as $file) {
                     $uniqueId = rand(10, 100000);
