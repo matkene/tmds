e9b799ce452efb41bab89d21e3e12897f70dd6e8
Robert Ebafua <robertebafua@gmail.com>
2021-08-27 13:36:04 +0100

    Booking Rule Changed

--- app/Http/Requests/CreateBookingRequest.php
+++ app/Http/Requests/CreateBookingRequest.php
@@ -25,31 +25,20 @@ class CreateBookingRequest extends FormRequest
     public function rules()
     {
         return [
-            'no_adults' => 'required|string',
-            'no_children' => 'required|string',
-            'no_infants' => 'required|string',
-            'ticket_no' => 'required|string',
-            'date_of_visit' => 'required|string',
-            'user_id' => 'required|string',
-            'tour_id' => 'required|string',
-        ];
-    }
-
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array
-     */
-    public function messages()
-    {
-        return [
-            'no_adults.required' => 'No of Adults is required',
-            'no_children.required' => 'No of Children is required',
-            'no_infants.required' => 'No of Infants is required',
-            'date_of_visit.required' => 'Date of Visit is required',
-            'ticket_no.required' => 'Ticket No  is required',
-            'user_id.required' => 'User Id is required',
-            'tour_id.required' => 'Tour Id is required',
+            'no_adult_male' => 'required',
+            'no_adult_female' => 'required',
+            'adult_option' => 'required',
+            'no_children_male' => 'required',
+            'no_children_female' => 'required',
+            'children_option' => 'required',
+            'no_infant_male' => 'required',
+            'no_infant_female' => 'required',
+            'infant_option' => 'required',
+            'user_id' => 'required',
+            'tour_id' => 'required',
+            'no_adult_sight_seeing' => 'required',
+            'no_children_sight_seeing' => 'required',
+            'date_of_visit' => 'required',
         ];
     }
 }
