56466310962d0afd61aed11e04c51f3a8bf3e81d
Robert Ebafua <robertebafua@gmail.com>
2021-09-15 12:19:10 +0100

    Api Modified

--- app/Repositories/ReportRepository.php
+++ app/Repositories/ReportRepository.php
@@ -8,13 +8,6 @@ class ReportRepository
 {
     public function allActivities()
     {
-        $allActivities = AuditLog::orderBy('created_at', 'DESC')->get();
-
-        return [
-            'logName' => $allActivities->log_name,
-            'description' => $allActivities->description,
-            'created_at' => $allActivities->created_at,
-            'updated_at' => $allActivities->updated_at,
-        ];
+        return AuditLog::orderBy('created_at', 'DESC')->get();
     }
 }
